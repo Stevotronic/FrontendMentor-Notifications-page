5e7f12c5b3d7cfc23e03008623d15a888b3612d7
Steves Playground <playground@mbp-van-steves.kpn>
2024-04-02 20:58:40 +0200

    version 1.0 with updated readme

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const notificationCounter = document.getElementById('notificationCounter');
 const newMessages = document.querySelectorAll('.newMessage');
 const bullets = document.querySelectorAll('.bullet');
 const removeClass = ()=>{
-    notificationCounter.style.display='none';
+    notificationCounter.innerHTML='0';
     newMessages.forEach(element => {
         element.classList.remove("newMessage");
     });
